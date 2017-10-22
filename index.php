9e3b1483c9540d5bfb22c5d0cf90f96210cae081
Shreejana Poudel <Sriz@users.noreply.github.com>
2017-10-21 22:46:41 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,166 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/design.css">
+	<link rel="stylesheet" href="css/normalize.css" />
+	<link rel="stylesheet" href="css/foundation.min.css" />
+	<link rel="stylesheet" href="css/app.css" />
+	
+<script src="js/vendor/custom.modernizr.js"></script>
+</head>
+<body style="background-image:url('back.jpg');">
+
+	<div class="row">
+		<div class="large-12 columns">
+			<nav class="top-bar">
+				<ul class="title-area" style="padding-top:0px;">
+					<li class="name"><img src="images/a-logo.png" height="55px" width="55px" style="padding-top:3px;"></li>
+					<li class="toggle-topbar menu-icon">
+						<a href="#"><span></span></a>
+					</li>
+				</ul>
+
+				<section class="top-bar-section" style="padding-top:0px;">
+					<ul class="left">
+						<li><a href="">Home</a></li>
+						<li><a href="">How it works</a></li>
+						<li class="has-dropdown"><a href="">Semester</a>
+						<ul class="dropdown">
+							<li>First</li>
+							<li>Second</li>
+							<li>Third</li>
+							<li>Fourth</li>
+							<li>Fifth</li>
+							<li>Sixth</li>
+							<li>Seventh</li>
+							<li>Eighth</li>
+						</ul>
+						</li>
+						<li><a href="">Best Assignments</a></li>
+						<li><a href="">About</a></li>
+					</ul>
+
+					<ul class="right" style="">
+          			<li class="search">
+            		<form>           
+            			<input type="search">
+            	    </form>
+            	    </li>
+
+          			<li class="has-button">
+           				<a class="tiny button" href="#">Search</a>
+          			</li>
+          		</ul>
+				</section>
+
+			</nav>
+		</div>
+	</div>
+	<div class="wrapper">
+		<div class="large-12 columns small-6">
+		<div class="white-line"></div>
+		<div class="line-break"></div>
+
+	
+		<div class="header">
+			<div class="image-ko">
+				<img src="images/copy-n-pencil.png" style="padding-left:20px; padding-top:20px;">
+			</div>
+			<div class="text">
+				<h1 style="padding-right:27px; padding-top:40px">Assignment Management System</h1>
+			</div>
+		</div>
+
+		
+	
+		<div class="content">
+			  <div class="row">    
+    
+    <!-- Main Content Section -->
+    <!-- This has been source ordered to come first in the markup (and on small devices) but to be to the right of the nav on larger screens -->
+    <div class="large-9 push-3 columns">
+      
+      
+      <div class="row">
+			<div class="large-12 columns">
+				<div class="row">
+					<div class="large-3 small-6 columns">
+						<img src="images/1.png" onmouseover="this.src='images/1-hover.png';" onmouseout="this.src='images/1.png';"/>
+						
+					</div>
+					<div class="large-3 small-6 columns">
+						<img src="images/2.png" onmouseover="this.src='images/2-hover.png';" onmouseout="this.src='images/2.png';"/>
+					</div>
+					<div class="large-3 small-6 columns">
+						<img src="images/3.png" onmouseover="this.src='images/3-hover.png';" onmouseout="this.src='images/3.png';"/>
+						
+					</div>
+					<div class="large-3 small-6 columns">
+						<img src="images/4.png" onmouseover="this.src='images/4-hover.png';" onmouseout="this.src='images/4.png';"/>
+					</div>
+				</div>
+				<br>
+				<div class="row">
+					<div class="large-3 small-6 columns">
+						<img src="images/5.png" onmouseover="this.src='images/5-hover.png';" onmouseout="this.src='images/5.png';"/>
+						
+					</div>
+					<div class="large-3 small-6 columns">
+						<img src="images/6.png" onmouseover="this.src='images/6-hover.png';" onmouseout="this.src='images/6.png';"/>
+						
+					</div>
+					<div class="large-3 small-6 columns">
+						<img src="images/7.png" onmouseover="this.src='images/7-hover.png';" onmouseout="this.src='images/7.png';"/>
+						
+					</div>
+					<div class="large-3 small-6 columns">
+						<img src="images/8.png" onmouseover="this.src='images/8-hover.png';" onmouseout="this.src='images/8.png';"/>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+      
+    </div>
+    
+    
+    <!-- Nav Sidebar -->
+    <!-- This is source ordered to be pulled to the left on larger screens -->
+    <div class="large-3 pull-9 columns">
+        
+      <ul class="side-nav">
+        <li><a href="#">B. Sc. CSIT</a></li>
+        <li><a href="#">BIM</a></li>
+        <li><a href="#">BSW</a></li>
+        <li><a href="#">BBS</a></li>
+        <li><a href="#">Intermediate</a></li>
+        <li><a href="#">A-Level</a></li>
+      </ul>
+      
+      <blockquote>"A very interactive application for both teachers and students."<cite>Vishnu Kumar Rana</cite></blockquote>
+        
+    </div>
+    
+  </div>
+
+		</div>
+		<div class="line-break"></div>
+		<div class="white-line"></div>
+	
+		<div class="content-footer">
+			<img src="images/comp-geek.png" height="270px" width="300px" style="padding-top:20px; padding-left:10px;">
+		</div>
+		<div class="footer">
+			<a href="">Home | </a>
+			<a href="">How it works | </a>
+			<a href="">Semester | </a>
+			<a href="">Best Assignments</a>
+			<div class="copy">2014 &copy; SyncSynergy45</div>
+		</div>
+	</div>
+
+</body>
+</html>
+
+
+
---
+++ progress.php
@@ -0,0 +1,58 @@
+	<link rel="stylesheet" href="css/progress-bar.css" type="text/css">
+
+	
+	<ul>
+	<li class="bar">
+		<h2>Course Progress</h2>
+		
+		<!-- WT -->
+    <p style="width:80%" data-value="80">Web Technologies</p>
+		<progress max="100" value="80" class="html5">
+			<!-- Browsers that support HTML5 progress element will ignore the html inside `progress` element. Whereas older browsers will ignore the `progress` element and instead render the html inside it. -->
+			<div class="progress-bar">
+				<span style="width: 80%">80%</span>
+			</div>
+		</progress>
+		
+		<!-- compiler -->
+    <p style="width:60%" data-value="60">Compiler</p>
+		<progress max="100" value="60" class="css3">
+			<!-- Browsers that support HTML5 progress element will ignore the html inside `progress` element. Whereas older browsers will ignore the `progress` element and instead render the html inside it. -->
+			<div class="progress-bar">
+				<span style="width: 60%">60%</span>
+			</div>
+		</progress>
+		
+		<!-- RTS -->
+    <p style="width:50%" data-value="50">Real Time System</p>
+		<progress max="100" value="50" class="jquery">
+			<!-- Browsers that support HTML5 progress element will ignore the html inside `progress` element. Whereas older browsers will ignore the `progress` element and instead render the html inside it. -->
+			<div class="progress-bar">
+				<span style="width: 50%">50%</span>
+			</div>
+		</progress>
+		
+	
+		
+    <!-- Net Centric -->
+    <p style="width:75%" data-value="75">Net Centric</p>
+		<progress max="100" value="75" class="python">
+			<!-- Browsers that support HTML5 progress element will ignore the html inside `progress` element. Whereas older browsers will ignore the `progress` element and instead render the html inside it. -->
+			<div class="progress-bar">
+				<span style="width: 75%">75%</span>
+			</div>
+		</progress>
+		
+		<!-- E-commerce -->
+    <p style="width:65%" data-value="65">E-commerce</p>
+		<progress max="100" value="65" class="php">
+			<!-- Browsers that support HTML5 progress element will ignore the html inside `progress` element. Whereas older browsers will ignore the `progress` element and instead render the html inside it. -->
+			<div class="progress-bar">
+				<span style="width: 65%">65%</span>
+			</div>
+		</progress>
+		
+	</li>
+</ul>
+
+		
